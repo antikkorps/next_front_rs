b58f1d7b40d844cc90343c9af113bb62c65ef3ee
Franck <franckvienot7@gmail.com>
2023-12-26 11:36:01 +0100

    added darkmode to the project

--- src/components/MoreDropdown.tsx
+++ src/components/MoreDropdown.tsx
@@ -8,19 +8,36 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  Settings,
+  Activity,
+  Bookmark,
+  Moon,
+  Sun,
+  LogOut,
+  ChevronLeft,
+} from "lucide-react"
+
 import { Button } from "./ui/button"
+import { Switch } from "@/components/ui/switch"
+import { Label } from "@/components/ui/label"
+
+import { cn } from "@/lib/utils"
 import { Menu } from "lucide-react"
 import { useState, useEffect, useRef } from "react"
+import { useTheme } from "next-themes"
 
 function MoreDropdown() {
+  const [showModeToggle, setShowModeToggle] = useState(false)
   const [open, setOpen] = useState(false)
   const ref = useRef<HTMLDivElement>(null)
+  const { theme, setTheme } = useTheme()
 
   useEffect(() => {
     function handleOutsideClick(event: MouseEvent) {
       if (!event.target) return
       if (ref.current && !ref.current.contains(event.target as Node)) {
-        // setShowModeToggle(false)
+        setShowModeToggle(false)
         setOpen(false)
       }
     }
@@ -45,17 +62,69 @@ function MoreDropdown() {
           </Button>
         </DropdownMenuTrigger>
 
-        <DropdownMenuContent ref={ref}>
-          <DropdownMenuItem>
-            <DropdownMenuLabel>More</DropdownMenuLabel>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuItem>
+        <DropdownMenuContent
+          ref={ref}
+          className={cn("dark:bg-neutral-800 w-64 !rounded-xl !p-0 transition-opacity")}
+          align="end"
+          alignOffset={-40}
+        >
+          {!showModeToggle && (
+            <>
+              <DropdownMenuItem className="menuItem">
+                <Settings size={20} />
+                <p>Settings</p>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="menuItem">
+                <Activity size={20} />
+                <p>Your activity</p>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="menuItem">
+                <Bookmark size={20} />
+                <p>Saved</p>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem
+                className="menuItem"
+                onClick={() => setShowModeToggle(true)}
+              >
+                <Moon size={20} />
+                <p>Switch appearance</p>
+              </DropdownMenuItem>
+
+              {/* <DropdownMenuItem className="menuItem" onClick={() => signOut()}>
+                <LogOut size={20} />
+                <p>Log out</p>
+              </DropdownMenuItem> */}
+            </>
+          )}
+
+          {showModeToggle && (
+            <>
+              <div className="flex items-center border-b border-gray-200 dark:border-neutral-700 py-3.5 px-2.5">
+                <ChevronLeft size={18} onClick={() => setShowModeToggle(false)} />
+                <p className="font-bold ml-1">Switch appearance</p>
+                {theme === "dark" ? (
+                  <Moon size={20} className="ml-auto" />
+                ) : (
+                  <Sun size={20} className="ml-auto" />
+                )}
+              </div>
+              <Label htmlFor="dark-mode" className="menuItem">
+                Dark Mode
+                <DropdownMenuItem className="ml-auto !p-0">
+                  <Switch
+                    id="dark-mode"
+                    className="ml-auto"
+                    checked={theme === "dark"}
+                    onCheckedChange={(checked) => {
+                      setTheme(checked ? "dark" : "light")
+                    }}
+                  />
+                </DropdownMenuItem>
+              </Label>
+            </>
+          )}
         </DropdownMenuContent>
       </DropdownMenu>
     </>
