a8fa65ca91ce11d3b352d46fea794d154243fb0d
Franck <franckvienot7@gmail.com>
2024-01-25 09:55:48 +0100

    corrected a visual bug

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <div className="w-20 flex-none lg:w-64 md:border-r">
         <SideNav />
       </div>
-      <div className="flew-grow mt-12 md:mt-0 flex-1 w-full md:overflow-y-auto sm:p-6 md:p-12 max-w-7xl mx-auto">
+      <div className="flex-grow mt-12 md:mt-0 flex-1 w-full md:overflow-y-auto sm:p-6 md:p-12 max-w-8xl mx-auto">
         {children}
       </div>
     </div>
