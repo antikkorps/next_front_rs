2cf6864ec6405040d329fdd141cfa2be2127b9d4
Franck <franckvienot7@gmail.com>
2024-01-25 10:03:40 +0100

    added a state to control the bookmarked clicked

--- src/components/ui/CardActionBookmarkBtn.tsx
+++ src/components/ui/CardActionBookmarkBtn.tsx
@@ -1,22 +1,42 @@
-import React from "react"
+"use client"
+import { useState } from "react"
 
 export default function CardActionBookmarkBtn() {
+  const [isBookmarked, setIsBookmarked] = useState(false)
+
+  const handleBookmark = () => {
+    setIsBookmarked(!isBookmarked)
+  }
+
   return (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="w-6 h-6 mr-2 text-gray-400"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-        />
-      </svg>
+      <div onClick={handleBookmark}>
+        {isBookmarked ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            viewBox="0 0 24 24"
+            className="w-6 h-6 mr-2"
+          >
+            <path d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 mr-2 text-gray-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+            />
+          </svg>
+        )}
+      </div>
     </>
   )
 }
