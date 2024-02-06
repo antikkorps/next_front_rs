b755d390caabdcee215bf87ccb01a783057b8497
Franck <franckvienot7@gmail.com>
2024-02-06 16:23:00 +0100

    added API_ENDPOINTS url to .env && add a .env.example && console/log the signin url on dashboard to check

--- src/app/[locale]/dashboard/(overview)/page.tsx
+++ src/app/[locale]/dashboard/(overview)/page.tsx
@@ -1,8 +1,12 @@
 import Card from "@/components/Card"
 import CardDetail from "@/components/CardDetail"
 import Stories from "@/components/Stories"
+import { API_ENDPOINTS } from "../../../../../configs/apiEndpoints"
 
 export default function DashboardPage() {
+  const login = API_ENDPOINTS.LOGIN
+  console.log("url de login", login)
+
   return (
     <>
       <div className="w-full sm:w-1/2 flex justify-center mx-auto mb-10 ">
